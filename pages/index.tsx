fcdfef431b7a84edf044254a4fb42ecd37a1655f
DaeJongLee <rarongo@naver.com>
2024-07-24 17:32:34 +0900

    초기설정

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Home: React.FC = () => {
+  return (
+    <Layout>
+      <h1>재고 관리 앱</h1>
+      {/* 여기에 재고 목록 및 추가 기능을 구현할 예정입니다 */}
+    </Layout>
+  );
+};
+
+export default Home;
